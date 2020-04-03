fe439661363f298cb25ed53698500a1c1f4bb78e
Aleksandra <aalexandrova11437@gmail.com>
2020-04-03 21:30:49 +0200

    IceCream House model added
    Created and added model of ice cream house with door open animation

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public Animator DoorAnimator;
+    void Start()
+    {
+        DoorAnimator = GetComponent<Animator>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            DoorAnimator.SetBool("isOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        DoorAnimator.SetBool("isOpen", false);
+    }
+
+}

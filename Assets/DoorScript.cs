969acde67090bfea244791554db2cf22ed5fdbdc
Aleksandra <aalexandrova11437@gmail.com>
2020-04-18 00:34:06 +0200

    Pause and Game Over menu + door animation fix

--- Assets/DoorScript.cs
+++ Assets/DoorScript.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class DoorScript : MonoBehaviour
 {
     public Animator DoorAnimator;
-    public Collider collider;
+
     void Start()
     {
         DoorAnimator = GetComponent<Animator>();
     }
     void OnTriggerEnter(Collider other)
     {
-        other = collider;
+
         if (other.gameObject.tag == "Player")
         {
             DoorAnimator.SetBool("isOpen", true);
@@ -21,7 +21,7 @@ public class DoorScript : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        other = collider;
+     
        // other.gameObject.GetComponent<Animator>().SetBool("isOpen", false);
         if(other.gameObject.tag =="Player")
         DoorAnimator.SetBool("isOpen", false);
---
+++ Assets/PauseMenuScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public GameObject Camera;
+    private Vector3 startingPosition;
+    public GameObject GameOver;
+    void Start()
+    {
+        PausePanel.SetActive(false);
+        GameOver.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            PausePanel.SetActive(true);
+            GetComponent<movementScript>().enabled = false;
+            Camera.GetComponent<CameraFollow>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+        }
+        if (Input.GetKeyDown("p")) { 
+        //{
+        //    startingPosition = Camera.transform.position;
+        //    Camera.transform.position = new Vector3(startingPosition.x, 50f);
+            PausePanel.SetActive(true);
+            GetComponent<movementScript>().enabled = false;
+            Camera.GetComponent<CameraFollow>().enabled = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        //if (GameOver.active == true)
+        //{
+        //    GetComponent<movementScript>().enabled = false;
+        //    Camera.GetComponent<CameraFollow>().enabled = false;
+        //    Cursor.lockState = CursorLockMode.None;
+        //    Cursor.visible = true;
+        //}
+    }
+}
